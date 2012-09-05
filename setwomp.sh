d422cce8927040d2a1a20655453c2bc3fec0e3d9
Rasmus Sten <rasmus@dll.nu>
2012-09-05 21:57:09 +0300

    allow installation into/from any directory

--- setwomp.sh
+++ setwomp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -xe
 
 basedir=`dirname $0`
+mydir=$( cd $basedir ; pwd )
 if [ "$1" = "install" ] ; then
-    sudo cp ${basedir}/nu.dll.womp.plist /Library/LaunchDaemons
+    sed -e "s/%WOMPDIR%/$mydir/g" < ${basedir}/nu.dll.womp.plist.in > nu.dll.womp.plist
+    sudo cp nu.dll.womp.plist /Library/LaunchDaemons/
     sudo chown root:wheel /Library/LaunchDaemons/nu.dll.womp.plist
     sudo launchctl load /Library/LaunchDaemons/nu.dll.womp.plist
     exit 0
